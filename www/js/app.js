6ba99e57d9163dc3e268b6470224a72ee2ceddae
mohamamd almidani <mhd.mam@gmail.com>
2015-06-09 15:59:19 +0800

    cropping image, init resize code

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'PhotoSnitch' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('PhotoSnitch', ['ionic', 'PhotoSnitch.controllers', 'PhotoSnitch.services'])
+angular.module('PhotoSnitch', ['ionic', 'PhotoSnitch.controllers', 'PhotoSnitch.services', 'images-resizer', 'angular-img-cropper'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers/MainCtrl.js
+++ www/js/controllers/MainCtrl.js
@@ -1,18 +1,52 @@
 angular.module('PhotoSnitch.controllers')
 
-.controller('MainCtrl', function($scope, CameraService) {
+.controller('MainCtrl', function($scope, CameraService, resizeService, MainService) {
 	$scope.image = {
 		src: 'img/default.png',
-		hasImage: false
+		hasImage: false,
+		isCropping: false,
+		srcCropped: null
 	};
 
 	$scope.loadImage = function() {
-		$scope.image.src = 'https://itspak.files.wordpress.com/2011/10/green-natural-scenes-31.jpg';
+		$scope.image.src = 'img/test.jpg';
 		$scope.image.hasImage = true;
 		// CameraService.getImage().then(function(img) {
 		// 	$scope.image.src = img;
 		// $scope.image.hasImage = true;
 		// });
+	};
+
+	$scope.resize = function() {
+		resizeService.resizeImage($scope.image.src, {
+			size: 100, // resolution
+			height: 500,
+			width: 500,
+			sizeScale: 'ko',
+		}, function(err, image) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+
+			//Add the resized image into the 
+			$scope.image.src = image;
+
+		});
+	};
+	$scope.crop = function() {
+		$scope.image.isCropping = true;
 	}
+
+	$scope.cancelCrop =function () {
+		$scope.image.isCropping = false;
+	}
+
+	$scope.doneCrop = function() {
+		$scope.image.src = $scope.image.srcCropped;
+		$scope.image.srcCropped = null;
+		$scope.image.isCropping = false;
+	}
+
 
 });
